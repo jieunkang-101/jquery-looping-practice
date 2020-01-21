c91dde10edeb09d783c4e2b81319e326ea3e4733
Student <example@example.com>
2020-01-21 10:51:17 -0800

    added jquery reductions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
-jQuery(document).ready(function() {
-    jQuery("h1").click(function() {
-        alert("This is a header.");
-        alert("I told you, THIS IS A HEADER!");
-  });
-  
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-  
-  jQuery("img").click(function() {
-    alert("This is an image.");
-  });
-}); 
+$(document).ready(function() {
+  // var elementArray = [];
+   $(document).click(function(event) {
+    //  console.log("element clicked!", this);
+    var header = "h1";
+    var paragraph = "p";
+    var image = "img";
+    //event.stopPropagation();
+    var elements = ["h1", "p", "img"];
+    // var domElement = event.target.nodeName;
+    // elementArray.push(domElement);
+    
+    // console.log("get", domElement.nodeName);
+    // console.log("event.target.nodeName", event.target.nodeName);
+
+   elements.forEach(function(element) {
+     console.log('This is a ' + element + ".")
+   });
+   });
+});
